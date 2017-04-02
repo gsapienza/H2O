72bfef080e058dc1bb4dd89f50312ade194a1a5a
Gregory Sapienza <Gregory@gregorys-mbp.home>
2017-04-02 12:57:59 -0400

    Core database migration to App Group

--- H2O/CoreDataStack.swift
+++ H2O/CoreDataStack.swift
@@ -3,61 +3,62 @@
 //  H2O
 //
 //  Created by Gregory Sapienza on 4/1/17.
-//  Copyright © 2017 Skyscrapers.IO. All rights reserved.
+//  Copyright © 2017 City Pixels. All rights reserved.
 //
 
 import CoreData
 
 class CoreDataStack {
-    // MARK: - Core Data stack
     
+    // MARK: - Core
+    
+    /// Application directory for SQL file. Returns the URL for the Documents directory if it exists, if it doesn't it returns the directory for the App Group.
     lazy var applicationDocumentsDirectory: URL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.theoven.H2O" in the application's documents Application Support directory.
-        
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return urls[urls.count-1]
-        //let urls = FileManager.default.urlsForDirectory(.documentDirectory, inDomains: .userDomainMask)
-        
-        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.H2O.ExtensionSharing")
-        
-        return url!
+        if self.migrationNeeded {
+            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            return urls[urls.count-1]
+
+        } else {
+            let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.H2O.ExtensionSharing")
+            return url!
+        }
     }()
     
+    /// Default Managed Object Model.
     lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = Bundle.main.url(forResource: "H2O", withExtension: "momd")
         return NSManagedObjectModel(contentsOf: modelURL!)!
     }()
     
+    /// Coordinator to work between context and store. If the coordinator finds the SQL file in the documents directory, it will migrate it over to the App Group directory.
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        
-        self.migrateCoreDataDatabaseToAppGroup(coordinator: coordinator)
-        
         
         let url = self.applicationDocumentsDirectory.appendingPathComponent("H2O.sqlite")
         var failureReason = "There was an error creating or loading the application's saved data."
+        
         do {
             try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true])
         } catch {
-            // Report any error we got.
             var dict = [String: AnyObject]()
             dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject
             dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject
             
             dict[NSUnderlyingErrorKey] = error as NSError
             let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
             abort()
+        }
+        
+        if self.migrationNeeded {
+            self.migrateCoreDataDatabaseToAppGroup(coordinator: coordinator)
         }
         
         return coordinator
     }()
     
+    
+    /// Default managed object context.
     lazy var managedObjectContext: NSManagedObjectContext = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
@@ -66,28 +67,61 @@ class CoreDataStack {
         return managedObjectContext
     }()
     
+    // MARK: - Migration to App Group
+    
+    /// Migrates a coordinator with a store in the Documents directory to the App Group directory.
+    ///
+    /// - Parameter coordinator: Coordinator to migrate.
     func migrateCoreDataDatabaseToAppGroup(coordinator: NSPersistentStoreCoordinator) {
-        guard let newURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.H2O.ExtensionSharing") else {
+        
+        // New URL for app group.
+        guard let newURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.H2O.ExtensionSharing")?.appendingPathComponent("H2O.sqlite") else {
             print("NewURL is nil")
             return
         }
         
+        // Old URL in documents directory.
         guard let oldUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("H2O.sqlite") else {
             print("OldURL is nil")
             return
         }
         
+        // Store in Documents directory.
         guard let store = coordinator.persistentStore(for: oldUrl) else {
             print("Store is nil")
             return
         }
         
+        // Other old URLs for files in Documents directory.
+        let otherOldURLs = [
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("H2O.sqlite-shm"),
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("H2O.sqlite-wal")
+            ]
+
+        
+        // Migrates store to App Group, removes the old SQL file and other old related files in Documents directory.
         do {
             try coordinator.migratePersistentStore(store, to: newURL, options: nil, withType: NSSQLiteStoreType)
+            try FileManager.default.removeItem(at: oldUrl)
+            
+            for otherOldURL in otherOldURLs {
+                if let otherOldURL = otherOldURL {
+                    try FileManager.default.removeItem(at: otherOldURL)
+                }
+            }
         } catch {
             print(error)
         }
     }
+    
+    /// True if a migration of the database from Documents to App Group needed.
+    lazy var migrationNeeded: Bool = {
+        guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("H2O.sqlite") else {
+            return false
+        }
+        
+        return FileManager.default.fileExists(atPath: url.path)
+    }()
     
     // MARK: - Core Data Saving support
     
